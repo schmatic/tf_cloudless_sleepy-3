d86619f8531ff85282435c5dbbfe12f13d846bb9
MT2017055 <ajreddy548@gmail.com>
2019-09-17 13:23:44 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -10,7 +10,15 @@ variable "sleepy_time" {
   default     = 548
 }
 
-variable "wakeup_time" {
-  description = "How long our local-exec will be awake."
-  default     = 1000000
+variable cloud_engineer_access {
+  description = "List of users and access policies for access_group: cloud_engineer"
+  default = {
+    "user"         = "eng1@us.ibm.com,eng2@us.ibm.com", // no space between 2 users.
+    "postgresql"   = "Viewer",
+    "cos"          = "Writer",
+    "kms"          = "Writer",
+    "cms"          = "Writer",
+    "vpc"          = "Editor",
+    "account_role" = "Developer"
+  }
 }
