c023da509953bf8f78fd8b094fa68c839713f558
schmatic <52877366+schmatic@users.noreply.github.com>
2022-10-09 20:12:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}"
 
-  vars {
-    sample_var = "${var.sample_var}"
+  vars = {
+    sample_var = var.sample_var
   }
 }
 
 resource "null_resource" "sleep" {
-  triggers {
-    uuid = "${uuid()}"
+  triggers = {
+    uuid = uuid()
   }
 
   provisioner "local-exec" {
